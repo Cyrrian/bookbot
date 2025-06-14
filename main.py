34fbc7204b2c6340655ffd34d199cfeeda5975b6
Cyrrian <william.r.stewart@gmail.com>
2025-06-13 21:01:40 -0500

    Added print_report function

--- main.py
+++ main.py
@@ -13,16 +13,36 @@ def load_book(file):
     return book
 
 #******************************
+# Function: print_report
+#******************************
+def print_report(file, word_count, char_counts):
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char in char_counts:
+        if  char["char"].isalpha():
+            print (f"{char['char']}: {char['num']}")
+
+    print("============= END ===============")
+
+#******************************
 # Function: main
 #******************************
 def main ():
+    file = "books/frankenstein.txt"
 
-    book = load_book("./books/frankenstein.txt")
+    book = load_book(file)
 
     words = get_word_count(book)
     chars = sort_char_counts(get_char_counts(book))
 
-    print(f"{words} words found in the document")
-    print(chars)
+    print_report(file, words, chars)
+
+    #print(f"{words} words found in the document")
+    #print(chars)
 
 main()
